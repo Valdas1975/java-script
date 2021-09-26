203be84a0b88a33c8ace1fa99ec3f08d38b016ef
Valdas1975 <60390963+Valdas1975@users.noreply.github.com>
2021-09-26 12:57:47 +0300

    Add files via upload

---
+++ java script/js/script.js
@@ -0,0 +1,16 @@
+console.log('hello word10');
+
+
+//pirmas variantas
+console.log('mokausi js');
+
+//antras variantas
+console.log('mokausi');
+console.log('js');
+
+//trecias variantas 
+console.log('mokausi '); console.log(' js')
+
+//ketvirtas variantas
+console.log('js');
+console.log('mokausi');
