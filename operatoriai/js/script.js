bcda719a916e928a6ddf4cc47968348e7f1a135e
Valdas1975 <60390963+Valdas1975@users.noreply.github.com>
2021-09-27 21:39:52 +0300

    Add files via upload

---
+++ operatoriai/js/script.js
@@ -0,0 +1,57 @@
+let userAge = 46;
+let userinfo = "Valdas Klimas";
+console.log(userAge,userinfo)
+
+let userHeigth = 176/0;
+console.log(userHeigth);
+
+let userName;
+console.log(typeof userName);
+
+ let userSize = "64" / "8";
+ console.log(typeof userSize);
+
+ let resultOne = "Valdas" +  " " + "bando" + " " +"mokytis" +" "+"javascript";//butinai reikia palikti sitoj vietoje tarpa" "
+ console.log(resultOne);
+
+ let resultTwo = "Valdas" + " "+ 46;
+ console.log(resultTwo);
+ console.log(typeof resultTwo);
+
+ let resultTrys = 46 + " " + "Valdas" ;
+ console.log(resultTrys);
+
+ let resulKet = 2+"2";
+ console.log(resulKet);//Kazusas
+
+ let resulPenki = 66-20+ " "+ "Valdas";
+ console.log(resulPenki);
+
+ let user="25";
+ let admin="35";
+ console.log(user+admin);//su vienu pliu sudetis su zodziais nesigaus
+
+ console.log(+user+ +admin);
+
+let users="15";
+let admins="31"
+ console.log(Number (users) + Number (admins));
+
+ let usersCounter=(8+3, 19+19);// cia visalaika rodis paskutini rezultata
+ console.log(usersCounter);
+
+ console.log(6>2);
+
+ console.log(2<4);
+
+ console.log('a'<'n');
+
+ console.log('z'>'n');
+
+ console.log('Valdas'<'Klimas');
+
+ console.log('Valdas'=='valdas');
+
+ console.log('Valdas'=='Valdas');
+
+
